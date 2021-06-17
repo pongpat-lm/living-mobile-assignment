a98ceac52ef3473be9f53f52c41320b3c2ca697b
big_pongpat <pongpat@livingmobile.me>
2021-06-17 18:59:20 +0700

    create and read menu

--- backend-rest/src/menu/menu.service.ts
+++ backend-rest/src/menu/menu.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { MenuModel } from './menu.model'; 
+import { CreateMenuDto } from './dto/createMenu.dto';
 
 @Injectable()
-export class MenuService {}
+export class MenuService {
+    constructor(
+        @InjectModel(MenuModel) // inject model and use it as repository
+        private menuRepo: typeof MenuModel, // UserModel act like userRepo here.
+      ) {}
+    
+      create(menu: CreateMenuDto) {
+          // userRepo is Sequelize model it have many functions to work with database.
+          // more info please see below documents.
+          return this.menuRepo.create(menu);
+      }
+    
+      findAll() {
+          return this.menuRepo.findAll();
+      }
+}
