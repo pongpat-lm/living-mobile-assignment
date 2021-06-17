9e78c3bb6729b4707137e090a7574a0651e68dc0
big_pongpat <pongpat@livingmobile.me>
2021-06-17 18:31:35 +0700

    install swagger

--- backend-rest/src/menu/menu.module.ts
+++ backend-rest/src/menu/menu.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { MenuModel } from './menu.model'; 
+import { MenuService } from './menu.service';
 
 @Module({
     imports: [SequelizeModule.forFeature([MenuModel])],
     exports: [SequelizeModule],
+    providers: [MenuService],
 })
 export class MenuModule {}
