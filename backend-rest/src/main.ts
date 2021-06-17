45a5bb2f370cd11728762174508c17f1ae6358c6
big_pongpat <pongpat@livingmobile.me>
2021-06-17 17:30:05 +0700

    create and read endpoint

--- backend-rest/src/main.ts
+++ backend-rest/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // config Swagger document's descriptions
+  const config = new DocumentBuilder()
+    .setTitle('Store')
+    .setVersion('1.0')
+    .build();
+
+  // create Swagger document
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document); // setup Swagger document at route "/api"
   await app.listen(3000);
 }
 bootstrap();
