c5787ce16409c817270e4cccc53f849d9a94c16f
big_pongpat <pongpat@livingmobile.me>
2021-06-17 14:55:48 +0700

    valid create

--- backend-rest/src/store/dto/createStore.dto.ts
+++ backend-rest/src/store/dto/createStore.dto.ts
@@ -1,12 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNumber, Min, Max } from 'class-validator';
 
 export class CreateStoreDto {
     @ApiProperty()
+    @IsString()
     name: string;
 
     @ApiProperty()
+    @IsString()
     description: string;
 
     @ApiProperty()
+    @IsNumber()
+    @Min(0)
+    @Max(10)
     rating: number;
 }
