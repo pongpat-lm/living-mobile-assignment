f67bf4e7246d44b31cf01dcb363c4a43d2dd86c9
big_pongpat <pongpat@livingmobile.me>
2021-06-17 16:50:42 +0700

    install dependencies

--- backend-rest/src/category/category.module.ts
+++ backend-rest/src/category/category.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { CategoryModel } from './category.model';
+import { CategoryService } from './category.service';
 @Module({
     imports: [SequelizeModule.forFeature([CategoryModel])],
     exports: [SequelizeModule],
+    providers: [CategoryService],
 })
 export class CategoryModule {}
