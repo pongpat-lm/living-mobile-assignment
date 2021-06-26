989820f0e6299eb4b87ea916ed325f5f37bcaf9e
big_pongpat <pongpat@livingmobile.me>
2021-06-26 16:39:53 +0700

    add vuex

--- frontend-backoffice/src/main.js
+++ frontend-backoffice/src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import './plugins/element.js'
 import "../src/assets/css/reset.css"
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
