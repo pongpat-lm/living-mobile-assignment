557f9e49edf36d9eeb6a3cb6c11c5fe5230ac431
big_pongpat <pongpat@livingmobile.me>
2021-06-26 21:27:06 +0700

    add form add and edit

--- frontend-backoffice/src/components/category/CategoryContent.vue
+++ frontend-backoffice/src/components/category/CategoryContent.vue
@@ -3,7 +3,11 @@
     <el-row class="header" type="flex" justify="space-between">
       <el-col class="name" :span="5">{{ name }}</el-col>
       <el-col class="button" :span="5"
-        ><el-button type="primary" round icon="el-icon-plus"
+        ><el-button
+          type="primary"
+          round
+          icon="el-icon-plus"
+          @click="toAdd"
           >Add New {{ name }}</el-button
         ></el-col
       >
@@ -19,19 +23,100 @@
         <el-table-column prop="storeName" label="Store Name"></el-table-column>
         <el-table-column label=" ">
           <div class="operation">
-            <img src="../../assets/Edit.png" alt="edit" class="edit">
-            <img src="../../assets/Copy.png" alt="copy" class="copy">
-            <img src="../../assets/Delete.png" alt="delete" class="delete">
+            <el-button type="text" @click.native="toEdit"
+              ><img
+                src="../../assets/Edit.png"
+                alt="edit"
+                class="edit"
+                @click="toEdit"
+            /></el-button>
+            <el-button type="text">
+              <img src="../../assets/Copy.png" alt="copy" class="copy" />
+            </el-button>
+            <el-button type="text">
+              <img src="../../assets/Delete.png" alt="delete" class="delete" />
+            </el-button>
           </div>
         </el-table-column>
       </el-table>
+    </div>
+    <!-- edit-form -->
+    <div class="edit-form" v-if="clickEditForm">
+      <el-dialog title="Edit Category" :visible.sync="clickEditForm">
+        <el-form :model="form" :rules="rules" ref="formEdit">
+          <el-form-item
+            label="Category name"
+            :label-width="labelWidth"
+            prop="name"
+          >
+            <el-input v-model="form.name" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item
+            label="Store name"
+            :label-width="labelWidth"
+            prop="storeName"
+          >
+            <el-select
+              v-model="form.storeName"
+              placeholder="Please select a zone"
+            >
+              <el-option label="Zone No.1" value="shanghai"></el-option>
+              <el-option label="Zone No.2" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button type="text" @click="clickEditForm = false"
+            >Cancel</el-button
+          >
+          <el-button type="primary" @click="submitForm('formEdit')" round
+            >Confirm</el-button
+          >
+        </span>
+      </el-dialog>
+    </div>
+    <!-- add-form -->
+    <div class="add-form" v-else-if="clickAddForm">
+      <el-dialog title="Add Category" :visible.sync="clickAddForm">
+        <el-form :model="form" :rules="rules" ref="formAdd">
+          <el-form-item
+            label="Category name"
+            :label-width="labelWidth"
+            prop="name"
+          >
+            <el-input v-model="form.name" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item
+            label="Store name"
+            :label-width="labelWidth"
+            prop="storeName"
+          >
+            <el-select
+              v-model="form.storeName"
+              placeholder="Please select a zone"
+            >
+              <el-option label="Zone No.1" value="shanghai"></el-option>
+              <el-option label="Zone No.2" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button type="text" @click="clickAddForm = false"
+            >Cancel</el-button
+          >
+          <el-button type="primary" @click="submitForm('formAdd')" round
+            >Confirm</el-button
+          >
+        </span>
+      </el-dialog>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Content",
+  name: "CategoryContent",
+  components: {},
   props: {
     name: {
       type: String,
@@ -40,6 +125,58 @@ export default {
     tableData: {
       type: Array,
       default: () => [],
+    },
+  },
+  data() {
+    return {
+      labelWidth: "120px",
+      clickEditForm: false,
+      clickAddForm: false,
+      form: {
+        name: "",
+        storeName: "",
+      },
+      rules: {
+        name: [
+          {
+            required: true,
+            message: "Please input Category name",
+            trigger: "blur",
+          },
+          {
+            min: 1,
+            message: "This field should not be empty.",
+            trigger: "blur",
+          },
+        ],
+        storeName: {
+          required: true,
+          message: "Please Select Store name",
+          trigger: "blur",
+        },
+      },
+    };
+  },
+  methods: {
+    toEdit() {
+      this.clickAddForm = false;
+      this.clickEditForm = true;
+      console.log("click edit");
+    },
+    toAdd() {
+      this.clickAddForm = true;
+      this.clickEditForm = false;
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        console.log("valid");
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
     },
   },
 };
---
+++ frontend-backoffice/src/components/category/CategoryEdit.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-dialog title="Edit Store" :visible.sync="isShow">
+    <el-form :model="form">
+      <el-form-item label="Promotion name" :label-width="120">
+        <el-input v-model="form.name" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="Zones" :label-width="120">
+        <el-select v-model="form.region" placeholder="Please select a zone">
+          <el-option label="Zone No.1" value="shanghai"></el-option>
+          <el-option label="Zone No.2" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="dialogFormVisible = false">Cancel</el-button>
+      <el-button type="primary" @click="dialogFormVisible = false"
+        >Confirm</el-button
+      >
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+    name: "CategoryEdit",
+    props: {
+      isShow: {
+        type: Boolean,
+        default: false
+      }
+    }
+};
+</script>
+
+<style>
+</style>
