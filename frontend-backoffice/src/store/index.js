4880a59f4d196314359778b8b5239e487ca07d7d
big_pongpat <pongpat@livingmobile.me>
2021-06-27 19:59:15 +0700

    install vuex

--- frontend-backoffice/src/store/index.js
+++ frontend-backoffice/src/store/index.js
@@ -5,10 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    stores: [],
+    categories: []
   },
   mutations: {
+    addCategory(state, {payload}) {
+      state.categories.push(payload)
+    }
   },
   actions: {
+    addCategory({ commit }, payload) {
+      commit("addCategory", { payload });
+    },
+  },
+  getters: {
+    stores: state => state.stores,
+    categories: state => state.categories,
   },
   modules: {
   }
