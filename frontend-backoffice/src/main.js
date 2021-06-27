bed69842b972bddbfb45ae772256e6c8738c6706
big_pongpat <pongpat@livingmobile.me>
2021-06-27 19:54:13 +0700

    install axios

--- frontend-backoffice/src/main.js
+++ frontend-backoffice/src/main.js
@@ -2,10 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
 import "../src/assets/css/reset.css"
+import VueAxios from "vue-axios";
+import axios from "axios";
+
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios);
 
 new Vue({
   router,
